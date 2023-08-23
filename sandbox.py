8256f22401c185ae3ba38db05022102b47737454
Yoni <yoni.stoller@gmail.com>
2023-08-23 17:20:11 +0300

    added back noise augmentation

--- sandbox.py
+++ sandbox.py
@@ -10,7 +10,6 @@ import config as c
 from data_preprocessing_vton.pose import PoseModel
 from data_preprocessing_vton.schp import extract_person_without_clothing
 import torch
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
